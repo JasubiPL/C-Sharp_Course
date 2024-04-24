3686b49dfedfc3dc03869096158b5ba794550f2a
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-04-24 09:54:57 -0600

    [Pildoras-Informaticas]:Clases he instancias de Clase

---
+++ 01_POO-Clases-Instancias/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Area de un circulo");
+
+
+Circulo miCirculo;
+
+miCirculo = new Circulo(); //Instancia de clase
+
+Console.WriteLine(miCirculo.calculoArea(5));
+
+Circulo miCirculo2 = new Circulo();// Forma corta de instanciar 
+
+Console.WriteLine(miCirculo2.calculoArea(9));
+
+class Circulo
+{
+    const double pi = 3.1416; //Propiedad
+
+    public double calculoArea(int radio) //Metodo de clase
+    {
+        return pi * radio * radio;
+    }
+
+}
