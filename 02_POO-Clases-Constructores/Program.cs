cff7627c6c8276ae7f625c81efe1fbf08dc0fc51
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-04-26 10:08:49 -0600

    [Pildoras-Informaticas]:Constructores de Clase

---
+++ 02_POO-Clases-Constructores/Program.cs
@@ -0,0 +1,38 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Coche coche1 = new Coche();
+Coche coche2 = new Coche();
+
+Console.WriteLine(coche1.getInfoCoche());
+
+Coche coche3 = new Coche(3.0, 2.0);
+Console.WriteLine(coche3.getInfoCoche());
+
+class Coche
+{
+    public Coche()
+    {
+        ruedas = 4;
+        largo = 2.3;
+        ancho = 1.7;
+        
+    }
+
+    public Coche(double largoCoche, double anchoCoche)
+    {
+        ruedas = 4;
+        largo = largoCoche;
+        ancho = anchoCoche;
+    }
+
+    public String getInfoCoche()
+    {
+        return "Info del Coche: \n Ruedas: " + ruedas + " Ancho: " + ancho + " Largo: " + largo;
+    }
+
+    private int ruedas;
+    private double largo;
+    private double ancho;
+    private bool climatizador;
+}
