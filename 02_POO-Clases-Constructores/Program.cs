18c7f0aa54c596e736ea192754559e190b5d7d75
Jasubi Piñeyro <gameportal7@hotmail.com>
2024-04-29 09:39:41 -0600

    [Pildoras-Informaticas]:getters, setter y parcial

--- 02_POO-Clases-Constructores/Program.cs
+++ 02_POO-Clases-Constructores/Program.cs
@@ -9,14 +9,20 @@ Console.WriteLine(coche1.getInfoCoche());
 Coche coche3 = new Coche(3.0, 2.0);
 Console.WriteLine(coche3.getInfoCoche());
 
-class Coche
+Coche coche4 = new Coche();
+
+coche4.setExtras(true, "tela");
+Console.WriteLine(coche4.getExtras());
+
+partial class Coche
 {
     public Coche()
     {
         ruedas = 4;
         largo = 2.3;
         ancho = 1.7;
-        
+        tapiceria = "tela";
+
     }
 
     public Coche(double largoCoche, double anchoCoche)
@@ -24,15 +30,34 @@ class Coche
         ruedas = 4;
         largo = largoCoche;
         ancho = anchoCoche;
+        tapiceria = "Licra";
     }
+
+}
+
+partial class Coche { 
 
     public String getInfoCoche()
     {
         return "Info del Coche: \n Ruedas: " + ruedas + " Ancho: " + ancho + " Largo: " + largo;
     }
 
+    public void setExtras(bool climatizador, String tapiceria)
+    {
+        this.climatizador = climatizador;
+        this.tapiceria = tapiceria;
+
+        
+    }
+
+    public String getExtras()
+    {
+        return "Tiene climatizador: " + climatizador + " Tipo de tapiceria: " + tapiceria;
+    }
+
     private int ruedas;
     private double largo;
     private double ancho;
     private bool climatizador;
+    private String tapiceria;
 }
